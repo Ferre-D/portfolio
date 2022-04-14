89d4f5f0a28a435a05142d4255ddcdfedc2b413e
Ferre-D <r0784245@student.thomasmore.be>
2022-04-14 07:47:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// check if page is ready jquery
+$(document).ready(function () {
+  // check if scrolled past button
+  let scrolledPast = false;
+  let button = $("#contact-me-button");
+
+  $(window).scroll(function () {
+    if (button.offset().top + button.outerHeight() < $(window).scrollTop()) {
+      if (scrolledPast) return;
+      scrolledPast = true;
+      $(".contact-pop-up-button").removeAttr("hidden");
+    } else {
+      if (!scrolledPast) return;
+      scrolledPast = false;
+      $(".contact-pop-up-button").attr("hidden", true);
+    }
+  });
+});
