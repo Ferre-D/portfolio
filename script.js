c491d8c34d887332e928311997d6d0f52fd4f166
Ferre-D <r0784245@student.thomasmore.be>
2022-04-17 14:36:07 +0200

    button animation on scroll

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 // check if page is ready jquery
 $(document).ready(function () {
   // check if scrolled past button
-  let scrolledPast = false;
+  let buttonWrapper = $(".button-wrapper");
   let button = $("#contact-me-button");
+  let buttonDiv = $(".home-button");
+  let scrolledPast =
+    buttonDiv.offset().top + buttonDiv.outerHeight() < $(window).scrollTop();
 
   $(window).scroll(function () {
-    if (button.offset().top + button.outerHeight() < $(window).scrollTop()) {
+    if (
+      buttonDiv.offset().top + buttonDiv.outerHeight() - 56 <
+      $(window).scrollTop()
+    ) {
       if (scrolledPast) return;
       scrolledPast = true;
-      $(".contact-pop-up-button").removeAttr("hidden");
+      buttonDiv.addClass("floating");
+      $("#not-floating-content").hide();
+      $("#floating-content").show();
+
+      // $(".contact-pop-up-button").removeAttr("hidden");
     } else {
       if (!scrolledPast) return;
       scrolledPast = false;
-      $(".contact-pop-up-button").attr("hidden", true);
+      buttonDiv.removeClass("floating");
+      $("#floating-content").hide();
+      $("#not-floating-content").show();
+      // $(".contact-pop-up-button").attr("hidden", true);
     }
   });
   $("nav li a").click((e) => {
     $("nav li.active").removeClass("active");
-    $(e.target).closest("li").addClass("active")
-  })
-  
+    $(e.target).closest("li").addClass("active");
+  });
 });
