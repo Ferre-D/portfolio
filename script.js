6c2832359cf280b6c25f70dfaf92ee75b3389aa9
Ferre-D <r0784245@student.thomasmore.be>
2022-05-19 00:50:46 +0200

    my internship text done

--- script.js
+++ script.js
@@ -2,20 +2,11 @@
 $(document).ready(function () {
   // check if scrolled past button
   $('[data-toggle="tooltip"]').tooltip();
-  let buttonWrapper = $(".button-wrapper");
-  let button = $("#contact-me-button");
   let buttonDiv = $(".home-button");
   let scrolledPast =
     buttonDiv.offset().top + buttonDiv.outerHeight() < $(window).scrollTop();
   $("#floating-content").hide();
 
-
-
-  let hideImageCover = (cover) => {
-      $(cover).removeClass("show-image-cover")
-      $(cover).addClass("hide-image-cover")
-      $(cover).siblings(".project-image").find("lottie-player").get(0).pause();
-    }
   let checkPosition = () => {
     if (
       buttonDiv.offset().top + buttonDiv.outerHeight() - 56 <
@@ -35,17 +26,19 @@ $(document).ready(function () {
     }
   };
   $(".contact-me-button").click((e) => {
-    alert("The site is currently under construction... \n You can always reach me on donne.ferre@gmail.com or shoot me a message on linkedin/ferre-donne")
-  })
-  $(".close-project-cover").click((e) => {
-    let cover = $(e.target).parent(".image-cover")
-    hideImageCover(cover);
-  })
+    alert(
+      "The site is currently under construction... \n You can always reach me on donne.ferre@gmail.com or shoot me a message on linkedin/ferre-donne"
+    );
+  });
   let checkNav = () => {
     $(".active").removeClass("active");
     if ($("#projects").offset().top - 56 < $(window).scrollTop()) {
       $(".active").removeClass("active");
       return $("#projects-nav").addClass("active");
+    }
+    if ($("#internship").offset().top - 56 < $(window).scrollTop()) {
+      $(".active").removeClass("active");
+      return $("#internship-nav").addClass("active");
     }
     if ($("#whatido").offset().top - 56 < $(window).scrollTop()) {
       return $("#whatido-nav").addClass("active");
@@ -56,11 +49,6 @@ $(document).ready(function () {
     }
   };
   checkPosition();
-  $("lottie-player").click((e) => {
-    $(e.target).parent(".project-image").siblings(".image-cover").removeClass("hide-image-cover")
-    $(e.target).parent(".project-image").siblings(".image-cover").addClass("show-image-cover")
-    $(e.target).get(0).play();
-  })
   $(window).scroll(function () {
     checkPosition();
     checkNav();
